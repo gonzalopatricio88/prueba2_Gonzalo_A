57a86434dcaaec9aac4225df2229536cded8db8d
Gonzalo <gonzalopatricio88@gmail.com>
2016-04-08 20:46:08 -0300

    new model employee & test:employee must have a name

---
+++ app/models/employee.rb
@@ -0,0 +1,5 @@
+class Employee < ActiveRecord::Base
+
+  validates :name, presence: true
+
+end
---
+++ test/models/employee_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class EmployeeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test 'employee must have a name' do
+
+    employee = employees(:one)
+    employee.name = nil
+    assert employee.valid?, 'employee must be a name'
+
+  end
+end
